5932c455a59e071c07572571506449ca8a8924db
Doug Park <dougparkarosa@gmail.com>
2023-10-03 21:34:58 -0700

    Created PeriodicTimer demonstration

---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+#include <Arduino.h>
+
+// Class to wrap up a call back function
+// that will be called periodically.
+
+// Define a CallBack as a function pointer
+// that takes no arguments and has no return
+// value.
+using CallBack = void (*)();
+
+class PeriodicTimer {
+ public:
+  using TickType = unsigned long;
+  static const TickType TicksPerSecond = 1000000;
+
+  PeriodicTimer(CallBack callback, unsigned long callDeltaMicros)
+      : mCallback(callback),
+        mStartMicros(micros()),
+        mCallDeltaMicros(callDeltaMicros) {}
+
+  // Call tick() as frequently as possible.
+  // There is overhead to this call probably on the order of
+  // 4 to 10 micro seconds. Longer if it calls the callback.
+  // When the callDeltaMicros time has elapsed the callback
+  // function will be called and the timing will be reset.
+  void tick() {
+    auto currentTime = micros();
+    if (currentTime - mStartMicros > mCallDeltaMicros) {
+      mStartMicros = currentTime;
+      (*mCallback)();
+    }
+  }
+
+ private:
+  CallBack mCallback;
+  unsigned long mStartMicros;
+  unsigned long mCallDeltaMicros;
+};
+
+void setup() { Serial.begin(9600); }
+
+void countA() {
+  static int count = 0;
+  Serial.print("A = ");
+  Serial.println(count);
+  count++;
+}
+
+void countB() {
+  static int count = 0;
+  Serial.print("  B = ");
+  Serial.println(count);
+  count += 1;
+}
+
+const PeriodicTimer::TickType interval = PeriodicTimer::TicksPerSecond * .1;
+const PeriodicTimer::TickType intervalA = interval;
+const PeriodicTimer::TickType intervalB = interval * 10;
+
+PeriodicTimer counterA(countA, intervalA);
+PeriodicTimer counterB(countB, intervalB);
+
+void loop() {
+  counterA.tick();
+  counterB.tick();
+}
